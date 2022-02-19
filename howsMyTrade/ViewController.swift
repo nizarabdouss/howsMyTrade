1f4c17b01a6e2bbb1f87cb00a42f910ed84ffbd9
nizarabdouss <nizarabdouss@gmail.com>
2022-02-18 19:23:21 -0500

    first commit

---
+++ howsMyTrade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  howsMyTrade
+//
+//  Created by anonym on 2022-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
